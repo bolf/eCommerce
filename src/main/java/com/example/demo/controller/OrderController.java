3df523e85077845307f4631ac1bdf87d22f58688
bolf <bolf@li.ru>
2020-11-15 22:34:03 +0300

    logging is added

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -1,6 +1,9 @@
 package com.example.demo.controller;
 
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +20,7 @@ import com.example.demo.repository.UserRepository;
 @RestController
 @RequestMapping("/api/order")
 public class OrderController {
+	private static final Logger log = LoggerFactory.getLogger(OrderController.class);
 	@Autowired
 	private UserRepository userRepository;
 	@Autowired
@@ -26,10 +30,12 @@ public class OrderController {
 	public ResponseEntity<UserOrder> submit(@PathVariable String username) {
 		User user = userRepository.findByUsername(username);
 		if(user == null) {
+			log.error("user not found: ".concat(username));
 			return ResponseEntity.notFound().build();
 		}
 		UserOrder order = UserOrder.createFromCart(user.getCart());
 		orderRepository.save(order);
+		log.info("submitted order for user: ".concat(username));
 		return ResponseEntity.ok(order);
 	}
 	
@@ -37,8 +43,10 @@ public class OrderController {
 	public ResponseEntity<List<UserOrder>> getOrdersForUser(@PathVariable String username) {
 		User user = userRepository.findByUsername(username);
 		if(user == null) {
+			log.error("order not found for user: ".concat(username));
 			return ResponseEntity.notFound().build();
 		}
+		log.info("got orders for user: ".concat(username));
 		return ResponseEntity.ok(orderRepository.findByUser(user));
 	}
 }
--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import com.example.demo.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,7 @@ import com.example.demo.model.requests.CreateUserRequest;
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
 	@Autowired
 	private UserService userService;
 
@@ -23,25 +26,34 @@ public class UserController {
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User user = userService.findById(id);
 		if(user == null){
+			log.error("user not found by Id: ".concat(String.valueOf(id)));
 			return ResponseEntity.badRequest().build();
 		}
+		log.info("user found by id: ".concat(String.valueOf(id)));
 		return ResponseEntity.ok(user);
 	}
 	
 	@GetMapping("/{username}")
 	public ResponseEntity<User> findByUserName(@PathVariable String username) {
 		User user = userService.findByUsername(username);
-		return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
+		if(user == null){
+			log.error("user not found by name: ".concat(username));
+			return ResponseEntity.notFound().build();
+		}
+		log.info("user found by name: ".concat(username));
+		return ResponseEntity.ok(user);
 	}
 	
 	@PostMapping("/create")
 	public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
 		if(createUserRequest.getPassword().length() < 7 ||
 				!createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())){
+			log.error("user was not created with bad password:".concat(createUserRequest.getPassword()));
 			return ResponseEntity.badRequest().build();
 		}
 		User user = new User(createUserRequest.getUsername(),createUserRequest.getPassword());
 		userService.registerUser(user);
+		log.info("user created: ".concat(createUserRequest.getUsername()));
 		return ResponseEntity.ok(user);
 	}
 	
